e3bc3becfc0ccda1bd352c26dabf4bb8f65f5a56
Art G <theartofnoise@gmail.com>
2019-04-25 12:39:19 -0400

    feat: responsive

---
+++ docs/precache-manifest.f7163eaf868bdac33ff41c5a11cb3cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b174bfd898b5de0296e7b4a1f6bc54",
+    "url": "/theartofnoise/Total-React-Recall/index.html"
+  },
+  {
+    "revision": "8d5cd3af387f1fe78ce4",
+    "url": "/theartofnoise/Total-React-Recall/static/css/main.494f5beb.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/theartofnoise/Total-React-Recall/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8d5cd3af387f1fe78ce4",
+    "url": "/theartofnoise/Total-React-Recall/static/js/main.44ef0e55.chunk.js"
+  },
+  {
+    "revision": "9f25d9bf5a22ecd82dbd",
+    "url": "/theartofnoise/Total-React-Recall/static/js/runtime~main.fa11fee2.js"
+  }
+]);
